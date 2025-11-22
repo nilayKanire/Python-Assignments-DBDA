867e24e5e585928f5b19926289cc6a3a66b3cc83
Nilay Kanire <139779307+nilayKanire@users.noreply.github.com>
2025-11-23 01:03:07 +0530

    done

--- Python Assignment 2/3 - Dictionary Assignment.py
+++ Python Assignment 2/3 - Dictionary Assignment.py
@@ -2,7 +2,7 @@
 #
 # Sample input : list = [10, 20, 30, (40,50), 60]
 # Sample output = 3
-
+'''
 mylist = [10,20, 30, (40,50), 60]
 print(type(mylist[3]))
 count = 0
@@ -12,17 +12,22 @@ for i in mylist:
     else:
         count = count + 1
 print(count)
-
-# for i in mylist:
-#     if(type(i) )
-
+'''
 # 2) create a tuple to store 5 numbers and then sort it in ascending and descending order.
 
+tup = (1, 5, 3, 2, 4)
+# ascending
+asc_tup = sorted(tup)
+print(asc_tup)
 
+# descending
+desc_tup = sorted(tup,reverse=True)
+print(desc_tup)
 
 # 3) Write a Python program to find the repeated items of a tuple.
 
-tup = ( 1, 2, 3, 4, 5, 5, 3, 1)
+
+
 
 
 
